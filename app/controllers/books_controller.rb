4fae6f37faf29d51fecbc3473c72d8fbcc78dd5f
dmmwebcampaizawa <105425062+dmmwebcampaizawa@users.noreply.github.com>
2022-06-09 11:05:23 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,22 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     #データを受け取り新規登録するためのインスタンス作成
     @book = Book.new(book_params)
+    @books = Book.all
     #データをデータベースに保存するためのsaveメソッド実行
     if @book.save
+      flash[:notice] = "Book was successfully created."
       # 詳細画面へリダイレクト
       redirect_to book_path(@book.id)
     else
-      render :new
+      render :index
     end
   end
 
-
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -29,15 +28,25 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+  @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
+    
+    # book = Book.find(params[:id])
+    # book.update(book_params)
+    # flash[:notice] = "Book was successfully created."
+    # redirect_to book_path(book.id)
 
   def destroy
-    book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully created."
+    redirect_to '/books'
   end
 
 
